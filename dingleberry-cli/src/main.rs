dfa72ccdca0d40f71e99ac624883e30516834319
Caleb <caleb.ottohayes@gmail.com>
2023-08-08 22:27:20 +1000

    Fixed type in flag list. Added coding challenges project

--- dingleberry-cli/src/main.rs
+++ dingleberry-cli/src/main.rs
@@ -122,7 +122,7 @@ fn start_vm(args: RunArgs) {
                     "Strings" | "strings" => native_flags.strings = true,
                     "List" | "list" => native_flags.list = true,
                     "Runtime" | "runtime" => native_flags.runtime = true,
-                    "Coroutine" | "oroutine" => native_flags.coroutine = true,
+                    "Coroutine" | "coroutine" => native_flags.coroutine = true,
                     _ => {}
                 }
             }
